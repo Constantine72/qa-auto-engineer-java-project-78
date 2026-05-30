25621b8366d65dfb749981428f8434e87809db12
Constantine <foofightermail@gmail.com>
2026-05-30 14:52:58 +0300

    get ValidatorTest.java name back for CI check

--- app/src/test/java/hexlet/code/ValidatorTest.java
+++ app/src/test/java/hexlet/code/ValidatorTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class StringValidatorTest {
+class ValidatorTest {
 
     @Test
     void testRequiredValidation() {
