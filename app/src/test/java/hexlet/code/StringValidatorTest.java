76563004f7944a0d003b6dfa7c6642c34e936a77
Constantine <foofightermail@gmail.com>
2026-05-30 15:59:05 +0300

    fixed naming for String validator

--- app/src/test/java/hexlet/code/StringValidatorTest.java
+++ app/src/test/java/hexlet/code/StringValidatorTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class ValidatorTest {
+class StringValidatorTest {
 
     @Test
     void testRequiredValidation() {
